118e353434a96b126486d6aa8bb81d6fc2486882
Havret <h4vret@gmail.com>
2018-07-30 22:48:51 +0200

    Add Lighthouse

---
+++ src/Lighthouse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lighthouse
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lighthouseService = new LighthouseService();
+            lighthouseService.Start();
+            Console.WriteLine("Press Control + C to terminate.");
+            Console.CancelKeyPress += async (sender, eventArgs) =>
+            {
+                await lighthouseService.StopAsync();
+            };
+            lighthouseService.TerminationHandle.Wait(); 
+        }
+    }
+}
